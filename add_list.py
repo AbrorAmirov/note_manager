a865b77eb5b0af66bc747724b0c73ccb693ef049
Аброр <abroramirov567@gmail.com>
2025-01-26 02:19:29 +0800

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+headings = []
+headings1 = input("Введите первый заголовок")
+headings2 = input("Введите второй заголовок")
+headings3 = input("Введите третий заголовок")
+headings.append(headings1)
+headings.append(headings2)
+headings.append(headings3)
+print(headings)
